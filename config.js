2d73e29281cd6632d18e583a18270c4a02d02139
Loveyouuuu <86777154+Loveyouuuu@users.noreply.github.com>
2021-07-01 18:03:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Hellu cậu!',
     introDesc: `Tớ có điều muốn nói với cậu
-    điều mà từ lâu tớ giấu dím
+    điều mà từ lâu tớ giấu dím,một
     chuyện rất quan trọng
     cậu mún biết hong`,
     btnIntro: 'ok nèee',
-    title: 'Tớ Thích Cậu 😙',
-    desc: 'Làm người yêu tớ nhéeee ',
+    title: 'Tớ Thích Cậu,làm người yêu tớ nhéeee 😙',
+    desc: 'Thoát ra là đồng ý rùi nhaa ',
     btnYes: 'Tớ đồng ý,tớ cũng thích cậu <33',
     btnNo: 'Không nhaaa :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    question:'Câụ thích điểm gì ở tớ <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại vì cậu cute vlllll <33333333',
     mess: 'tớ biết mà 🥰. Yêu cậu nhiều nhiều, 😘😘',
     messDesc: 'Tối nay 7h tớ qua đón nhé bae.',
     btnAccept: 'Okiiiii lun <3',
