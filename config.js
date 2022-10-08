8b93d086481aa8d830de8e7a2ad564aca3c7cd63
Loveyouuuu <86777154+Loveyouuuu@users.noreply.github.com>
2022-10-08 20:54:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em yêu à!',
-    introDesc: `Chị biết em đang dỗi lắm
-    nhưng chị có vài lời muốn nói với em
-    mong em có thể tha thứ cho chị
-    em muốn nghe hong`,
+    introTitle: 'Hellu ny!',
+    introDesc: `Em có chuyện này 
+    rất quan trọng
+    muốn nói với chị
+    chị muốn nghe hong`,
     btnIntro: 'ok nèee',
-    title: 'Chị sai rồi,chị xin lỗi mà.em đừng dỗi nữa nhaaaa 😙',
+    title: 'Cưới em nhéeee ? 😙',
     desc: 'Thoát ra là đồng ý rùi nhaa ',
-    btnYes: 'em không dỗi nữa em yêu chị ❤️',
+    btnYes: 'chị đồng ý,chị yêu em ❤️',
     btnNo: 'Không nhaaa :3',
-    question:'chị tuyệt quá nên em k dỗi đc đúng k? ❤️',
-    btnReply: 'Gửi cho chị ❤️',
-    reply: 'Đúng rồi chị tuyệt vời nhất <33333333',
-    mess: 'Chị biết mà 🥰. Yêu Em nhiều nhiều,moazz 😘😘',
-    messDesc: 'Gửi ngàn nụ hôn đến em nhé baee.',
+    question:'Trên thế giới có hơn 7 tỉ người mà sao chị lại yêu em? ❤️',
+    btnReply: 'Gửi cho em ❤️',
+    reply: 'Vì em tuyệt vời nhấtttt <33333333',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều,moazz 😘😘',
+    messDesc: 'Gửi ngàn nụ hôn đến chị nhé baee.',
     btnAccept: 'Dạaaaaa <3',
     messLink: 'http://fb.com'//https://m.me/yenle014
 }
